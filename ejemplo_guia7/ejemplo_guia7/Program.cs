8e6f0b44d59aab9b0b4f90517eb74985ce699f8a
eabonilla <edal.bonilla@gmail.com>
2025-03-21 08:15:25 -0600

    Actualizacion de ejemplo con vectores

--- ejemplo_guia7/ejemplo_guia7/Program.cs
+++ ejemplo_guia7/ejemplo_guia7/Program.cs
@@ -1,28 +1,22 @@
 ﻿using System.Linq.Expressions;
 
-double dia1, dia2, dia3, dia4, dia5, dia6, dia7;
+int cantDias = 0;
+
+Console.WriteLine("Cuantos dias desea calcular la temperatura ");
+cantDias = int.Parse(Console.ReadLine());
+
+double[] dias = new double[cantDias];
+
 int opcion = 0;
 
-Console.WriteLine("Ingrese la temperatura del dia1");
-dia1 = Double.Parse(Console.ReadLine());
+for (int i = 0; i < dias.Length; i++)
+{
+    Console.WriteLine("Ingrese la temperatura del dia " + (i+1));
+    dias[i] = Double.Parse(Console.ReadLine());
+}
 
-Console.WriteLine("Ingrese la temperatura del dia2");
-dia2 = Double.Parse(Console.ReadLine());
 
-Console.WriteLine("Ingrese la temperatura del dia3");
-dia3 = Double.Parse(Console.ReadLine());
 
-Console.WriteLine("Ingrese la temperatura del dia4");
-dia4 = Double.Parse(Console.ReadLine());
-
-Console.WriteLine("Ingrese la temperatura del dia5");
-dia5 = Double.Parse(Console.ReadLine());
-
-Console.WriteLine("Ingrese la temperatura del dia6");
-dia6 = Double.Parse(Console.ReadLine());
-
-Console.WriteLine("Ingrese la temperatura del dia7");
-dia7 = Double.Parse(Console.ReadLine());
 
 do
 {
@@ -35,11 +29,11 @@ do
     switch (opcion)
     {
         case 1:
-            double resultado  = calcularPromedioTemp(dia1,dia2,dia3,dia4,dia5,dia6,dia7);
+            double resultado  = calcularPromedioTemp(dias);
 
             Console.WriteLine("Promedio de temp semanal: "+ resultado);
             break;
-        case 2:
+        /*case 2:
             Console.WriteLine("temperatura mas fria y la mas calurosa");
             double tempMaxima = dia1;
             double tempMin = dia2;
@@ -125,7 +119,7 @@ do
 
             Console.WriteLine("Porcentajes de dias menores a 0: " + (count / 7)*100 + "%");
 
-            break;
+            break;*/
         case 4: 
             break;
         default: 
@@ -140,15 +134,22 @@ do
 
 
 
-static double calcularPromedioTemp(double dia1, double dia2, double dia3, double dia4, double dia5, double dia6, double dia7 )
+double calcularPromedioTemp(double[] tempByday)
 {
+    double sumaTemp = 0;
+    for (int i = 0; i < tempByday.Length; i++)
+    {
+        sumaTemp += tempByday[i];
+    }
+
+
     Console.WriteLine("Promedio de temperatura semanal");
-    double promedio = (dia1 + dia2 + dia3 + dia4 + dia5 + dia6 + dia7) / 7;
+    double promedio = sumaTemp / tempByday.Length;
     return promedio;
 
 }
 
-static double tempMAx(double temp1, double temp2)
+double tempMAx(double temp1, double temp2)
 {   double temp_Max = 0;
     if (temp1 > temp2)
     {
